fc9b3c3fb4ec5848ddc2c256fe51e24a903ab783
Johann Prell <johann.prell@gmail.com>
2019-11-14 22:42:45 +0100

    recite randomised poem

--- src/index.js
+++ src/index.js
@@ -2,11 +2,35 @@ import * as AFRAME from 'aframe'
 import * as DB from './db'
 import randomInt from 'random-int'
 import 'aframe-text-geometry-component'
+import { init as initText, printMain } from './text'
 
-init()
+window.onload = init()
+let currentLine = -1
+let frameIndex = 0
+let printInterval = 200
+
+let currentPoem
 
 async function init() {
+  initText()
+  await getRandomPoem()
+  printLoop()
+}
+
+async function getRandomPoem() {
   const authors = await DB.getAuthorsByLetter('J')
-  const poem = await DB.getRandomPoemByAuthor(authors[randomInt(0, authors.length - 1)])
-  console.log(authors, poem)
+  currentPoem = await DB.getRandomPoemByAuthor(authors[randomInt(0, authors.length - 1)])
+  console.log(authors, currentPoem)
+}
+
+async function printLoop() {
+  if(++frameIndex % printInterval === 0) {
+    if(++currentLine < currentPoem.lines.length) {
+      printMain(currentPoem.lines[currentLine])
+    }
+    else {
+      await getRandomPoem()
+    }
+  }
+  requestAnimationFrame(printLoop)
 }
--- src/text.js
+++ src/text.js
@@ -1,3 +1,20 @@
 import * as AFRAME from 'aframe'
 import * as GEOTEXT from 'aframe-text-geometry-component'
 
+let mainText
+
+export function init() {
+  console.log('attempt to init main text entity...')
+  if(document.querySelector('#main-text')) {
+    mainText = document.querySelector('#main-text')
+    console.log('Main text entity init:', mainText)
+    printMain('')
+  }
+  else {
+    requestAnimationFrame(init)
+  }
+}
+
+export function printMain(text) {
+  mainText.setAttribute('text-geometry', { value: text })
+}
